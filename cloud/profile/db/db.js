e6d4dcb0d88d025d33e12df5619939a0e271096f
Bagus <bagus.nurhuda@ui.ac.id>
2022-05-16 18:54:43 +0700

    change name folder

--- cloud/profile/db/db.js
+++ cloud/profile/db/db.js
@@ -3,8 +3,8 @@ const { Pool } = require('pg');
 const pool = new Pool({
     user: "postgres",
     host:'localhost',
-	password: "fadil123",
-	database: "nyobatest",
+	password: "hudahuda",
+	database: "database1",
     port: 5432,
 });
 
