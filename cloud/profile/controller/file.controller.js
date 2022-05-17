48b5996bedc5cd7d2eb74a9fc55bdf553a314abb
Bagus <bagus.nurhuda@ui.ac.id>
2022-05-17 19:33:34 +0700

    change maksimum upload to 3 photos

--- cloud/profile/controller/file.controller.js
+++ cloud/profile/controller/file.controller.js
@@ -9,64 +9,88 @@ const bucket = storage.bucket("nyoba_project");
 
 
 const upload = async (req, res) => {
+            
     try {
         await processFile(req, res);
-        console.log(req);
+        
+        var counter = 0;
         if (!req.files) {
-            return res.status(400).send({ message: "Please upload a file" });
+            res.status(400).send('No file uploaded.');
+            return;
         }
-
-        
-
-        //create a new blob in the bucket and upload file
-        const blob = bucket.file(req.files[0].originalname);
-        
-        const blobStream = blob.createWriteStream({
-            resumable: false,
-        });
-        blobStream.on("error", (err) => {
-            res.status(500).send({ message: err.message });
-        });
-        blobStream.on("finish", async (data) => {
-            // membuat URL agar bisa diakses HTTP
-            const publicUrl = format(
-                `https://storage.googleapis.com/${bucket.name}/${blob.name}`
-            );
-
-            // try {
-            //     // membuat file menjadi public
-            //     await bucket.file(req.file.originalname).makePublic();
-            // } catch {
-            //     return res.status(500).send({
-            //         message:
-            //             `Uploaded the file successfully: ${req.file.originalname}, but public access is denied!`,
-            //         url: publicUrl,
-            //     });
-            // }
 
             nama_lengkap = req.body.nama_lengkap;
             wa = req.body.wa;
             alamat_tinggal = req.body.alamat_tinggal;
             alamat_ktp = req.body.alamat_ktp;
             profesi = req.body.profesi;
+            let foto_diri;
+            let foto_ktp;
+            let foto_selfie;
 
-            const query = `INSERT INTO profile(nama_lengkap, wa, alamat_tinggal, alamat_ktp, profesi, link_gambar) values ('${nama_lengkap}', '${wa}', '${alamat_tinggal}', '${alamat_ktp}', '${profesi}', '${publicUrl}');`
-            await db.query(query, (err, results) => {
-                if (err) {
-                    console.error(err.message);
-                    res.send(err.message);
-                    return;
-                  } else {
-                    console.log('data berhasil dimasukkan');
-                    return res.status(200).send({
-                        message: "Uploaded the file successfully: " + req.files[0].originalname,
-                        url: publicUrl,
-                    });
-                  }
+        let proms = new Promise((resolve, reject) => {
+           
+            req.files.forEach ( (fil) => {
+            const blob = bucket.file(fil.originalname);  
+            console.log(fil);
+            const blobStream = blob.createWriteStream({
+                resumable: false,
             });
-
-        });
-        blobStream.end(req.files.buffer);
+        
+            blobStream.on("error", (err) => {
+                res.status(500).send({ message: err.message });
+            });
+            console.log(blob.name);
+            const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`
+            if (counter === 0){
+                foto_diri = publicUrl 
+                console.log('sukses uplaod diri');
+                
+            } else if (counter ===1){
+                foto_ktp = publicUrl
+                console.log('sukses uplaod ktp');
+                
+            } else {
+                foto_selfie = publicUrl
+                console.log('sukses uplaod selfie');
+                
+                
+            }
+     
+            blobStream.end(fil.buffer);
+            counter+=1
+                if (counter ===3){
+                    resolve()
+                    
+                }
+            
+        })
+        })
+        
+        proms.then(async (message) => {
+            // Handle results
+            const query = `INSERT INTO profile(nama_lengkap, wa, alamat_tinggal, alamat_ktp, profesi, foto_diri, foto_ktp, foto_selfie) values ('${nama_lengkap}', '${wa}', '${alamat_tinggal}', '${alamat_ktp}', '${profesi}', '${foto_diri}', '${foto_ktp}', '${foto_selfie}');`
+                await db.query(query, (err, results) => {
+                    if (err) {
+                        console.error(err.message);
+                        res.send(err.message);
+                        return;
+                    } else {
+                        console.log('data berhasil dimasukkan');
+                        return res.status(200).send(
+                            `ini adalah foto diri ${foto_diri}
+                            ini adalah foto ktp ${foto_ktp}
+                            ini adalah foto selfie ${foto_selfie}`
+                        );
+                    }
+                });
+        })
+        .catch(e => {
+            console.error(e);
+        })
+        
+        
+        
     } catch (err) {
         console.log(err.message);
         if (err.code == "LIMIT_FILE_SIZE") {
@@ -75,7 +99,7 @@ const upload = async (req, res) => {
             });
         }
         res.status(500).send({
-            message: `Could not upload the file: ${req.files[0].originalname}. ${err.message}`,
+            message: `Could not upload the file: ${req.files.originalname}. ${err.message}`,
         });
     }
 };
