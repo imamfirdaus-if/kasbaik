8e5644f350e35982129dc05d99ab7e04435f50b9
m-krisnandi <mkrisnandi08@gmail.com>
2022-05-17 21:56:24 +0700

    add node_module

--- cloud/borrower/index.js
+++ cloud/borrower/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const borrowerRoutes = require('./src/borrowers/routes');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(express.json());
 
