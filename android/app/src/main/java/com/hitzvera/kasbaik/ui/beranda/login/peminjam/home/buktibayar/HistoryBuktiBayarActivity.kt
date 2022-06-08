1cf5424163d5f41958fac09dfb8141411274a2ce
Mujahid Ansori Majid <hitzveraidn@gmail.com>
2022-06-08 11:47:07 +0700

    making layout for upload bukti bayar

---
+++ android/app/src/main/java/com/hitzvera/kasbaik/ui/beranda/login/peminjam/home/buktibayar/HistoryBuktiBayarActivity.kt
@@ -0,0 +1,12 @@
+package com.hitzvera.kasbaik.ui.beranda.login.peminjam.home.buktibayar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hitzvera.kasbaik.R
+
+class HistoryBuktiBayarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history_bukti_bayar)
+    }
+}
