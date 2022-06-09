d579541a3a89be0f7397bbe62055072a93d3747e
FakhriFaishalRochdiana <thirdcrash30@gmail.com>
2022-06-09 17:25:33 +0700

    Slicing UI Payment Super Admin

---
+++ android/app/src/main/java/com/hitzvera/kasbaik/ui/beranda/login/superadmin/home/pembayaran/ListPaymentActivity.kt
@@ -0,0 +1,11 @@
+package com.hitzvera.kasbaik.ui.beranda.login.superadmin.home.pembayaran
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListPaymentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_payment)
+    }
+}
