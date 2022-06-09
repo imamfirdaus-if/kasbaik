0987f665fdad466aac2ddb4642cff4826aedf416
FakhriFaishalRochdiana <thirdcrash30@gmail.com>
2022-06-09 16:54:52 +0700

    Slicing UI Profile SuperAdmin

---
+++ android/app/src/main/java/com/hitzvera/kasbaik/ui/beranda/login/superadmin/home/profile/ProfileAdminActivity.kt
@@ -0,0 +1,12 @@
+package com.hitzvera.kasbaik.ui.beranda.login.superadmin.home.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hitzvera.kasbaik.R
+
+class ProfileAdminActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_admin)
+    }
+}
