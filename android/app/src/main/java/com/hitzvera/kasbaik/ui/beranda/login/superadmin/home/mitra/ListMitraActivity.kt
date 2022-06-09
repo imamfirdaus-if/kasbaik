d28c8b64b1f81448ceb1f0982f1502528d02d18c
FakhriFaishalRochdiana <thirdcrash30@gmail.com>
2022-06-09 17:15:11 +0700

    Slicing UI List

---
+++ android/app/src/main/java/com/hitzvera/kasbaik/ui/beranda/login/superadmin/home/mitra/ListMitraActivity.kt
@@ -0,0 +1,11 @@
+package com.hitzvera.kasbaik.ui.beranda.login.superadmin.home.mitra
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListMitraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_mitra)
+    }
+}
---
+++ android/app/src/main/java/com/hitzvera/kasbaik/ui/beranda/login/superadmin/home/pengguna/ListPenggunaActivity.kt
@@ -0,0 +1,11 @@
+package com.hitzvera.kasbaik.ui.beranda.login.superadmin.home.pengguna
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListPenggunaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_pengguna)
+    }
+}
