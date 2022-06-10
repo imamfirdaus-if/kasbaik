2a2e56a0d9b173c41e4d0def4ddefebaafdf4d94
FakhriFaishalRochdiana <thirdcrash30@gmail.com>
2022-06-10 08:39:19 +0700

    Slicing UI History Payment

---
+++ android/app/src/main/java/com/hitzvera/kasbaik/ui/beranda/login/superadmin/home/pembayaran/history/HIstoryPaymentAdminActivity.kt
@@ -0,0 +1,11 @@
+package com.hitzvera.kasbaik.ui.beranda.login.superadmin.home.pembayaran.history
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HIstoryPaymentAdminActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history_payment_admin)
+    }
+}
